bac8aea133e6a8b557daac05331ae61b36913e03
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-10-08 13:50:56 +0000

    [Add] usersのカラムにintroducion追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   resources :books
   resources :users
+  get 'homes/about', to: 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
