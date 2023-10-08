417ad16ffcf74e4110a8caf1e6e97d3a5ab2ead9
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-10-08 13:19:40 +0000

    [Add] bootstrap導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/index'
-  get 'users/edit'
-  get 'users/show'
   devise_for :users
   root to: 'homes#top'
   resources :books
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
