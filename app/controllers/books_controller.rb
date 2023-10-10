d1e548a0064a3ed28aa462814d328731d4f415f9
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-10-10 05:12:41 +0000

    [Add] UserInfo,Newbookの部分テンプレート完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,16 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book = Book.save
+    redirect_to users_path
   end
 
   def show
@@ -10,4 +18,12 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
