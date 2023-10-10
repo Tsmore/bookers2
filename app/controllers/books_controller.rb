2c64cbefd347173a96b0a97f4b8be6d1cd22f64f
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-10-10 13:24:50 +0000

    [Add/Fix]　いろいろおかしくなり修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,17 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
-  end
-
-  def new
     @book = Book.new
+    @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
-    @book.user_id = current_user_id
-    @book = Book.save
-    redirect_to users_path
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: 'Book was successfully created'
+    else
+      render :new
+    end
   end
 
   def show
@@ -19,13 +19,30 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to @book, notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: 'Book was successfully deleted'
   end
 
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body, :user_id)
   end
 
 end
