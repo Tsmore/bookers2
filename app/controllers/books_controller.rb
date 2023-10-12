2651d64b853dc20569fcbfc58f5b1d0a16e9944a
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-10-12 09:10:16 +0000

    [Fix]テスト結果から修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:index, :show, :edit]
 
   def index
     @book = Book.new
