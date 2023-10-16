c9e3ed8528834340107d3a7f3582c81582bec5da
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-10-16 10:32:42 +0000

    [Add] フォロー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :show, :update, :edit]
+  resources :users, only: [:index, :show, :update, :edit] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   get 'home/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
