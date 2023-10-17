6c090723c53250ad1f3d6737f26ca750bba5da0f
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-10-17 09:23:19 +0000

    [Add]コメントの非同期処理

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_back(fallback_location:root_path)
   end
 
   def destroy
-    comment = BookComment.find(params[:id])
-    comment.destroy
-    redirect_back(fallback_location:root_path)
+    @book_comment = BookComment.find(params[:id])
+    @book = @book_comment.book
+    @book_comment.destroy
+    @book_comment = BookComment.new
   end
 
   private
